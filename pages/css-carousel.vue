cd47ea752c0fb6095d75e7274ca2ab6a88fb318f
Michael Starks <michael.starks@bkwld.com>
2021-06-25 14:59:38 -0600

    adding some intersectionObserver fun

--- pages/css-carousel.vue
+++ pages/css-carousel.vue
@@ -6,11 +6,17 @@
 	h1 CSS Based Carousel
 
 	h2 Full width slides
-	.carousel-wrap.full-width
+	.carousel-wrap.full-width(ref='fullSlide')
 		.carousel(ref='slider')
 			.slide(v-for='num in 6' 
 				:key='num'
-				:num='num'): visual-slide(:num='num')
+				:num='num'
+				ref='fullSlides'): visual-slide(:num='num')
+			
+		.slider-navigation
+			.prev(v-if='currentIndex > 1' @click='goPrev') ←
+			.next(v-if='currentIndex < 6' @click='goNext') →
+
 		.pagination
 			.dot(v-for='num in 6' 
 				:key='num'
@@ -43,7 +49,8 @@
 		.carousel
 			.slide(v-for='num in 6' 
 				:key='num'
-				:num='num'): visual-slide(:num='num')
+				:num='num'): visual-slide(:num='num' 
+					variable-width)
 		.pagination
 			.dot(v-for='num in 6' 
 				:key='num'
@@ -59,13 +66,25 @@
 
 export default
 
-	data: -> currentIndex: 0
+	data: -> currentIndex: 1
+
+	mounted: ->
+		options =
+			root: @$refs.fullSlide
+			rootMargin: '0px'
+			threshold: 0.5
+		observer = new IntersectionObserver @checkSlide, options
+		for slide, i in @$refs.fullSlides
+			observer.observe slide
 
 	methods:
+		checkSlide: (entries, observer) ->
+			for entry in entries
+				if entry.isIntersecting
+					@currentIndex = entry.target.attributes.num.value
 
-		# Slider navigation
 		goNext: (index) ->
-			if @currentIndex < @media.length - 1 then @currentIndex++
+			if @currentIndex < 6 then @currentIndex++
 
 		goPrev: (index) ->
 			if @currentIndex > 0 then @currentIndex--
@@ -73,12 +92,12 @@ export default
 		jumpToSlide: (i) -> @currentIndex = i
 	
 	watch:
+	
 		currentIndex: (val) ->
 			@$refs.slider.scroll {
 				left: @$refs.slider.offsetWidth * (@currentIndex - 1)
 				behavior: 'smooth'
 			}
-
 
 </script>
 
@@ -90,7 +109,7 @@ export default
 	display none
 
 .carousel-wrap
-	overflow hidden
+	position relative
 	display flex
 	flex-wrap wrap
 	justify-content center
@@ -109,6 +128,28 @@ export default
 	overflow-x auto
 	scroll-snap-type x mandatory
 	scroll-behavior smooth
+
+.prev,
+.next
+	position absolute
+	top 50%
+	transform translateY(-50%)
+	width 50px
+	height 50px
+	border-radius 50%
+	box-shadow 0px 2px 5px rgba(0,0,0,0.5)
+	background white
+	display flex
+	justify-content center
+	align-items center
+	font-size 20px
+	cursor pointer
+
+.prev
+	left -20px
+
+.next
+	right -20px
 
 .pagination
 	display flex
